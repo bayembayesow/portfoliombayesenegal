d9f5c47151f6f949a717ec4114303458f2088b5f
bayembayesow <128776282+bayembayesow@users.noreply.github.com>
2023-07-07 23:00:58 -0400

    modification du portfolio pour utiliser react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
+import Accueil from '../pages/Accueil';
+import Projets from '../pages/Projets';
+import Contact from '../pages/Contact';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <main>
+        <Route path="/" exact component={Accueil} />
+        <Route path="/projets" component={Projets} />
+        <Route path="/contact" component={Contact} />
+      </main>
+      <Footer />
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <a href="https://github.com/votre-utilisateur">GitHub</a>
+        <a href="https://www.linkedin.com/in/votre-profil">LinkedIn</a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
